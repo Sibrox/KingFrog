1863f021314f159b435189c5df0cf12c156bac76
gallottino <d.gallotti@outlook.it>
2019-12-21 18:52:23 +0100

    Fix save cloud

--- Assets/Script/GameManager/GooglePlayServices.cs
+++ Assets/Script/GameManager/GooglePlayServices.cs
@@ -161,6 +161,11 @@ public class GooglePlayServices
 
                 loading = false;
             }
+            else
+            {
+                loading = false;
+                GameManager.instance.SetSave();
+            }
         }
         else
         {
